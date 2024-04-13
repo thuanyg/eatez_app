4b6f004e9ff4c6e1e160931a4b80e5f461a88090
Thuanyg <thuan2682k3@gmail.com>
2024-04-13 19:08:48 +0700

    Update by ThuanHT

--- app/src/main/java/com/thuanht/eatez/view/Fragment/LoginTabFragment.java
+++ app/src/main/java/com/thuanht/eatez/view/Fragment/LoginTabFragment.java
@@ -1,8 +1,10 @@
 package com.thuanht.eatez.view.Fragment;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,22 +23,27 @@ import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.thuanht.eatez.databinding.FragmentLoginTabBinding;
+import com.thuanht.eatez.interfaceEvent.LoginCallback;
+import com.thuanht.eatez.jsonResponse.LoginResponse;
 import com.thuanht.eatez.utils.KeyboardUtils;
 import com.thuanht.eatez.view.Activity.HomeActivity;
 import com.thuanht.eatez.view.Activity.LoginActivity;
 import com.thuanht.eatez.viewModel.LoginViewModel;
 import com.thuanht.eatez.R;
 
-public class LoginTabFragment extends Fragment {
+public class LoginTabFragment extends Fragment implements LoginCallback {
     private FragmentLoginTabBinding binding;
     private LoginViewModel viewModel;
     private FirebaseAuth mAuth;
+
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentLoginTabBinding.inflate(inflater, container, false);
         viewModel = new ViewModelProvider(requireActivity()).get(LoginViewModel.class);
+
         // Quan sát LiveData để nhận thông báo lỗi email & password
         viewModel.getEmailError().observe(requireActivity(), errorString -> {
             binding.txtEmail.setError(errorString);
@@ -45,7 +52,7 @@ public class LoginTabFragment extends Fragment {
             binding.textInputLayout.setEndIconMode(TextInputLayout.END_ICON_NONE);
             binding.txtEmail.requestFocus();
         });
-        eventHandler();
+
         return binding.getRoot();
     }
 
@@ -54,9 +61,33 @@ public class LoginTabFragment extends Fragment {
         binding.btnLogin.setOnClickListener(v -> {
             String email = binding.txtEmail.getText().toString();
             String password = binding.txtPassword.getText().toString();
+
+
             if (viewModel.validateData(email, password)) {
                 viewModel.Login(email, password);
+
             }
         });
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        viewModel.setLoginCallback(this); // Thiết lập loginCallback trong ViewModel
+        eventHandler(); // Gọi eventHandler() để thiết lập sự kiện cho nút đăng nhập
+    }
+
+    @Override
+    public void onLoginSuccess() {
+        Intent intent = new Intent(requireContext(), HomeActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onLoginFailure(String errorMessage) {
+        Toast.makeText(getContext(), errorMessage, Toast.LENGTH_SHORT).show();
+    }
+
+
+
 }
