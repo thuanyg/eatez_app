c1faf6b55b6e18f03cba2fb1a1ff47f3221da7f7
Thuanyg <thuan2682k3@gmail.com>
2024-04-13 22:40:58 +0700

    Update by ThuanHT

--- app/src/main/java/com/thuanht/eatez/LocalData/MySharedPreferences.java
+++ app/src/main/java/com/thuanht/eatez/LocalData/MySharedPreferences.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 
 public class MySharedPreferences {
     private static final String MY_SHARED_PREFERENCES = "MY_SHARED_PREFERENCES";
+    private static final String KEY_IS_LOGGED_IN = "IS_LOGGED_IN"; // Thêm key mới để lưu trạng thái đăng nhập
     private Context context;
 
     public MySharedPreferences(Context context) {
@@ -22,5 +23,14 @@ public class MySharedPreferences {
         SharedPreferences sharedPreferences = this.context.getSharedPreferences(MY_SHARED_PREFERENCES, Context.MODE_PRIVATE);
         return sharedPreferences.getBoolean(key, false);
     }
-
+    public void setLoggedIn(boolean isLoggedIn) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(MY_SHARED_PREFERENCES, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(KEY_IS_LOGGED_IN, isLoggedIn);
+        editor.apply();
+    }
+    public boolean isLoggedIn() {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(MY_SHARED_PREFERENCES, Context.MODE_PRIVATE);
+        return sharedPreferences.getBoolean(KEY_IS_LOGGED_IN, false);
+    }
 }
