1e417be36e44720695234430a9e19f4f0714e8a2
thuanht <thuan2682k3@gmail.com>
2024-04-24 16:42:43 +0700

    thuanht

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "Beta 1.0"
+        versionName = "Beta 1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
